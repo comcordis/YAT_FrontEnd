955d2721c376d932aa5e0c491db251991898e81c
bazmuz <sebasmuskens@gmail.com>
2014-04-15 10:27:24 +0200

    bas bugfixing

--- js/main.js
+++ js/main.js
@@ -32,8 +32,25 @@ $(document).ready(function() {
 		$('#tBD1').animate({ marginTop: '-100px', opacity: 0 }, 200);
 	});
 	
+	// modal
+	setTimeout(function() {$('#yatModal').modal();}, 2000);
 	
-	// google map
+	// acccordion faq
+    $('.accordion .accordionDetail').hide();
+       $('.accordion h1 a').click(function(){
+          if ($(this).hasClass('active')) {
+               $(this).removeClass('active');
+               $(this).parent().next().slideUp();
+          } else {
+               $('.accordion h1 a').removeClass('active');
+               $(this).addClass('active');
+               $('.accordion .accordionDetail').slideUp();
+               $(this).parent().next().slideDown();
+          }
+          return false;
+       });
+       
+    // google map
 	/*$('#mapView').gmap({
 		'center': '52.088561,5.3404',
 		'mapTypeId': google.maps.MapTypeId.HYBRID,
@@ -41,8 +58,5 @@ $(document).ready(function() {
 		'scrollwheel': false,
 		'disableDefaultUI':true
 	});*/
-	
-	// modal
-	setTimeout(function() {$('#yatModal').modal();}, 2000);
 	
 });
